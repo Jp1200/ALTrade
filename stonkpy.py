dc9e903d8972e36e0467d93c5a9b5c916e1c511a
Jp1200 <jprkr96@gmail.com>
2021-01-27 12:38:59 -0600

    added file reader

--- stonkpy.py
+++ stonkpy.py
@@ -4,13 +4,22 @@ import pprint
 import numpy
 
 # https://github.com/alpacahq/alpaca-trade-api-python/ 
+def read_keys(file_name):
+    f = open(file_name, 'r')
+    KEYS = f.readlines()
 
+    keys= []
+    for key in KEYS:
+        keys.append(key.strip())
+    f.close()
+    return keys[0], keys[1]
 
+API_KEY_PAPER, SECRET_PAPER = read_keys('keys_paper.txt')
 
 api = tradeapi.REST(API_KEY_PAPER, SECRET_PAPER, base_url='https://paper-api.alpaca.markets')
 
 def listPos(api):
-
+    pass
 
 account = api.get_account()
 print(api.list_positions())
